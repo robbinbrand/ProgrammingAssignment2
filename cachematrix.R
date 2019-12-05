f020b1d517a348b0c47b7d5f020bf8c36c9fb874
robbinbrand <58513348+robbinbrand@users.noreply.github.com>
2019-12-05 08:19:23 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Firtst put x as imput for the matrix
+## Than put q to solve valuas as null
+## Then every reference needs to solve for mean
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  q <- NULL
+  set <- function(y) {
+    x <<- y
+    q <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) q <<- solve
+  getsolve <- function() q
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## As before i changed/solved the mean, also the 'm' to 'q' 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(q)) {
+    message("getting_inversed_matrix")
+    return(q)
+  }
+  data <- x$get()
+  q <- solve(data, ...)
+  x$setsolve(q)
+  q
 }
+        ## Return a matrix that is the inverse of 'x'
+
+
